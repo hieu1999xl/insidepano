bd972c5a8432d455beef41fabdd8ceb5930d3bab
hiu1999xl <hiu99xl@gmail.com>
2022-09-13 21:05:38 +0700

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var panorama, viewer, container, infospot;
 
 container = document.querySelector( '#container' );
 
-panorama = new PANOLENS.ImagePanorama( './insidepano.jpg' );
+panorama = new PANOLENS.ImagePanorama( './images/insidepano.jpg' );
 
 infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
 infospot.position.set( 0, -2000, -5000 );

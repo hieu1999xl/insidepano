ecdc686f3c5bca1a2c0c2c9cdc78b083f8215ccb
hiu1999xl <hiu99xl@gmail.com>
2022-09-13 20:34:07 +0700

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var panorama, viewer, container, infospot;
+
+container = document.querySelector( '#container' );
+
+panorama = new PANOLENS.ImagePanorama( './insidepano.jpg' );
+
+infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
+infospot.position.set( 0, -2000, -5000 );
+
+panorama.add( infospot );
+
+viewer = new PANOLENS.Viewer( { container: container } );
+viewer.add( panorama );
+
+viewer.addUpdateCallback(function(){
+  
+});
